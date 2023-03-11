d5b8221edf218f666d06a07993afe2a4f7055ee9
Samuel Sunmonu <codebstack@gmail.com>
2023-03-11 03:13:50 +0100

    implemented smooth scrolling while navigation sections and some general fixes

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -81,7 +81,7 @@ const NavBar: React.FunctionComponent<
       className={`${
         navBg &&
         'bg-[rgba(131, 9, 0, 0.05)] backDrop'
-      } z-[100] px-[5%] w-full  fixed top-0 transition-all duration-300 ease-linear py-4 md:py-9 flex items-center justify-between font-SF1 text-nav-text text-[14px]`}
+      } z-[100] px-[5%] w-full  fixed top-0 transition-all duration-300 ease-linear py-4 md:py-6 flex items-center justify-between font-SF1 text-nav-text text-[14px]`}
     >
       <Logo />
       <div className="hidden gap-10 items-center md:flex">
