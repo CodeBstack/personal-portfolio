7b8ae9b580838ef91b4560de10cbbcb0cfa7c10a
Samuel Sunmonu <codebstack@gmail.com>
2023-03-31 15:07:01 +0100

    animated side bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -136,72 +136,72 @@ const NavBar: React.FunctionComponent<
       </div>
 
       {/* MOBILE NAV SIDEBAR */}
-
-      <aside
-        className={`flex md:hidden px-[7%] overflow-yauto transition-all duration-500 flex-col items-center z-[70] fixed top-0 h-screen bg-[#020A13] ${
-          isNavOpened
-            ? 'right-0 w-[70%]'
-            : '-right-[100vw]'
-        } `}
-      >
-        <IconButton
-          sx={{
-            position: 'absolute',
-            right: '5%',
-            top: '20px',
-          }}
-          onClick={() => setIsNavOpened(false)}
+     
+        <aside
+          className={`flex md:hidden px-[7%] overflow-yauto transition-all duration-500 flex-col items-center z-[70] fixed top-0 h-screen bg-[#020A13] ${
+            isNavOpened
+              ? 'right-0 w-[70%]'
+              : '-right-[100vw]'
+          } `}
         >
-          <CloseIcon />
-        </IconButton>
-
-        <div className="w-full flex mt-[104px] mb-12 gap-6 flex-col text-center">
           <IconButton
-            onClick={() =>
-              setIsDarkMode(!isDarkMode)
-            }
+            sx={{
+              position: 'absolute',
+              right: '5%',
+              top: '20px',
+            }}
+            onClick={() => setIsNavOpened(false)}
           >
-            {isDarkMode ? (
-              <NightsStayIcon
-                fontSize="medium"
-                sx={{ color: '#CCD6F6' }}
-              />
-            ) : (
-              <NightsStayOutlinedIcon
-                fontSize="medium"
-                sx={{ color: '#CCD6F6' }}
-              />
-            )}
+            <CloseIcon />
           </IconButton>
 
-          {NavLinks.map((navlink, i) => (
-            <AnchorLink
-              key={i}
-              className="hover:bg-primary py-2 rounded-[8px]"
-              href={`#${navlink.text.toLocaleLowerCase()}`}
-              onClick={() => {
-                setSelectedPage(
-                  navlink.text.toLocaleLowerCase()
-                );
-                setIsNavOpened(false);
-              }}
+          <div className="w-full flex mt-[104px] mb-12 gap-6 flex-col text-center">
+            <IconButton
+              onClick={() =>
+                setIsDarkMode(!isDarkMode)
+              }
             >
-              <p className="text-secondary mb-[4px]">
-                {navlink.pos}
-              </p>
-              <p>{navlink.text}</p>
-            </AnchorLink>
-          ))}
-        </div>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://drive.google.com/file/d/1JiCcfzAtOYUUes-SjT-jRfRkNyF4FBnd/view?usp=sharing"
-          className="w-full max-w-[152px] py-[18px] text-center border-2 border-secondary bg-transparent font-SF2 text-secondary rounded-[5px] text-sm self-center hover:bg-secondary hover:text-primary transition duration-500"
-        >
-          Resume
-        </a>
-      </aside>
+              {isDarkMode ? (
+                <NightsStayIcon
+                  fontSize="medium"
+                  sx={{ color: '#CCD6F6' }}
+                />
+              ) : (
+                <NightsStayOutlinedIcon
+                  fontSize="medium"
+                  sx={{ color: '#CCD6F6' }}
+                />
+              )}
+            </IconButton>
+
+            {NavLinks.map((navlink, i) => (
+              <AnchorLink
+                key={i}
+                className="hover:bg-primary py-2 rounded-[8px]"
+                href={`#${navlink.text.toLocaleLowerCase()}`}
+                onClick={() => {
+                  setSelectedPage(
+                    navlink.text.toLocaleLowerCase()
+                  );
+                  setIsNavOpened(false);
+                }}
+              >
+                <p className="text-secondary mb-[4px]">
+                  {navlink.pos}
+                </p>
+                <p>{navlink.text}</p>
+              </AnchorLink>
+            ))}
+          </div>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/1JiCcfzAtOYUUes-SjT-jRfRkNyF4FBnd/view?usp=sharing"
+            className="w-full max-w-[152px] py-[18px] text-center border-2 border-secondary bg-transparent font-SF2 text-secondary rounded-[5px] text-sm self-center hover:bg-secondary hover:text-primary transition duration-500"
+          >
+            Resume
+          </a>
+        </aside>
     </nav>
   );
 };
